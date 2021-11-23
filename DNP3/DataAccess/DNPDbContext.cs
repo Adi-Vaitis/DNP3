203f30446aadd107d5cf6df44958acf2d078c0e0
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2021-11-23 12:08:22 +0100

    Created DB Context class

---
+++ DNP3/DataAccess/DNPDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DNP3.Models;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DNP3.DataAccess
+{
+    public class DNPDbContext : DbContext
+    {
+        public DbSet<Adult> Adults { get; set; }
+        public DbSet<Family> Families { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = C:\Users\vaiti\Documents\GitHub\DNP3\DNP3\DNP3.db");
+        }
+    }
+}
